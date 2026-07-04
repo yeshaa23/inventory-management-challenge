ac74e1204a2029217799c1ba11a76484194fd5ef
yeshaa23 <ayeshanazkiya@gmail.com>
2026-07-04 23:29:16 +0700

    add ci coverage

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,16 @@
 <?php
 
-it('returns a successful response', function () {
+it('redirects homepage to login page', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect(route('login'));
+});
+
+it('can render login page', function () {
+    $response = $this->get('/login');
+
+    $response->assertOk();
+    $response->assertSee('Telkomsel Inventory');
+    $response->assertSee('Email');
+    $response->assertSee('Password');
 });
